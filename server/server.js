3ee92e9821d1beefdbad1fe0bf08fa0c56fdd866
qunnderrie1 <qunnderrie@gmail.com>
2024-11-29 19:28:17 -0500

    made some changes

--- server/server.js
+++ server/server.js
@@ -41,7 +41,7 @@ app.post("/", (req, res) => {
   });
 });
 
-const PORT = process.env.API_URL || 3001;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
